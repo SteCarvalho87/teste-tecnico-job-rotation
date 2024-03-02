a3c1cb38958338b4e2cc607a94df0d1834b48c5b
Stephanie Carvalho <ste.aoc@gmail.com>
2024-03-02 06:51:31 -0300

    commit do desafio 5

---
+++ 5 - string/string.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+
+    char palavra[80];
+    int i, j, tamanho;
+    char troca;
+
+    printf ("Digite uma palavra qualquer:\n");
+    fgets(palavra, sizeof(palavra),stdin);
+    tamanho = strlen(palavra);
+    
+    for(i=0, j=tamanho-1; i < tamanho/2; i++, j--)
+    {
+        troca = palavra[i];
+        palavra[i] = palavra[j];
+        palavra[j] = troca;
+    }
+    printf ("\n A palavra invertida fica assim: %s", palavra);
+
+    return 0;
+}
